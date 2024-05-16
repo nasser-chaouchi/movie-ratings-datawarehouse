d1489bfecfb53c053c3aa2f84ac32ba8ce8f00c7
nchaouch <nasser.hxh@outlook.fr>
2024-05-16 14:59:06 +0200

    Ajout tables sql

---
+++ procedures/sp_genre_dim_table.sql
@@ -0,0 +1,29 @@
+CREATE OR REPLACE PROCEDURE public.sp_genre_dim_table(
+	)
+LANGUAGE 'plpgsql'
+AS $BODY$
+BEGIN
+    MERGE INTO genre_dim_table AS target
+    USING (
+        SELECT DISTINCT LOWER(UNNEST(STRING_TO_ARRAY(m.genres, '|'))) AS genre_name
+        FROM (
+            SELECT movieid, STRING_AGG(genres, '|') AS genres
+            FROM public.movies
+            GROUP BY movieid
+        ) AS m
+    UNION
+        SELECT DISTINCT LOWER(UNNEST(STRING_TO_ARRAY(t.genres, ','))) AS genre_name
+        FROM (
+            SELECT tconst, STRING_AGG(genres, ',') AS genres
+            FROM public.title_basics
+            GROUP BY tconst
+        ) AS t
+    ) AS source
+    ON (LOWER(target.genre_name) = source.genre_name)
+    WHEN NOT MATCHED THEN
+        INSERT (genre_name)
+        VALUES (source.genre_name);
+END;
+$BODY$;
+ALTER PROCEDURE public.sp_genre_table()
+    OWNER TO postgres;
---
+++ tables/genre_dim_table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS public.genre_dim_table (
+    genre_key SERIAL PRIMARY KEY,
+    genre_name VARCHAR(255) 
+);
